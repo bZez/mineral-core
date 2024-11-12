764679893abf83ddfd75adc373f8084e205d46c0
PandaGuerrier <76222146+PandaGuerrier@users.noreply.github.com>
2024-11-12 21:37:06 +0100

    feat(voice): handle a lot of op codes, udp connection / close for prevent ghost udp

--- lib/src/infrastructure/internals/voice/wss/voice_wss.dart
+++ lib/src/infrastructure/internals/voice/wss/voice_wss.dart
@@ -2,10 +2,12 @@ import 'dart:convert';
 
 import 'package:mineral/container.dart';
 import 'package:mineral/src/api/common/voice/voice_state.dart';
+import 'package:mineral/src/infrastructure/internals/voice/udp/udp_tunnel.dart';
 import 'package:mineral/src/infrastructure/internals/voice/voice_controller.dart';
 import 'package:mineral/src/infrastructure/internals/voice/voice_manager.dart';
 import 'package:mineral/src/infrastructure/internals/voice/wss/constants/voice_op_code.dart';
 import 'package:mineral/src/infrastructure/internals/voice/wss/dispatchers/voice_authentication.dart';
+import 'package:mineral/src/infrastructure/internals/voice/wss/dispatchers/voice_heartbeat.dart';
 import 'package:mineral/src/infrastructure/internals/voice/wss/voice_message.dart';
 import 'package:mineral/src/infrastructure/services/wss/websocket_client.dart';
 
@@ -17,11 +19,14 @@ final class VoiceWss {
   late final WebsocketClient client;
   late final VoiceController controller;
   late final VoiceAuthentication authentication;
+  late final UDPTunnel udpTunnel;
+  late final VoiceHeartbeat heartbeat;
 
-  VoiceWss({required this.url, required this.state }) {
+  VoiceWss({required this.url, required this.state}) {
     name = 'voice-${state.serverId}';
     controller = ioc.resolve<VoiceManagerContract>().get(state.serverId);
     authentication = VoiceAuthentication(this, controller.kernel);
+    heartbeat = VoiceHeartbeat(this);
   }
 
   Future<void> init() async {
@@ -31,9 +36,14 @@ final class VoiceWss {
       onError: (error) {
         print('error $error');
       },
-      onClose: (int? exitCode) {
-        print('exitCode $exitCode');
+      onClose: (int? exitCode, String? reason) {
+        print('exitCode $exitCode, reason $reason');
         controller.disconnect();
+
+        // close udp tunnel
+
+        udpTunnel.receiver.close();
+        udpTunnel.sender.close();
       },
     );
 
@@ -48,6 +58,14 @@ final class VoiceWss {
         switch (code) {
           case VoiceOpCode.hello:
             authentication.identify(payload);
+          case VoiceOpCode.ready:
+            authentication.selectProtocol(payload);
+          case VoiceOpCode.sessionDescription:
+            authentication.handleSessionDescription(payload);
+          case VoiceOpCode.heartbeat:
+            heartbeat.heartbeat();
+          case VoiceOpCode.heartbeatAck:
+            heartbeat.ack(payload['t']);
           default:
             break;
         }
